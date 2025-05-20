f5b1e2ef07576e38cfb95a53cf7e5610137357c2
Punitsingh-1124 <punitsingh2413@gmail.com>
2025-05-20 21:42:21 +0530

    Recursion problems

---
+++ Recursion10/app.js
@@ -0,0 +1,125 @@
+//Recursion
+
+//Stack memory
+//Push and Pop
+//return keyword
+//Introduction on Recursion
+
+// exp: 1
+// greet()
+// function greet() {
+//     console.log("First");
+//     enjoy()
+// }
+// function enjoy() {
+//     console.log("second");
+//     temp()
+// }
+// function temp() {
+//     console.log("third");
+// }
+// First
+// second
+// third
+
+//exp:2
+// greet()
+// function greet() {
+//     let a = 10;
+//     console.log("First");
+//     enjoy()
+// }
+// function enjoy() {
+//     console.log("second");
+//     temp()
+// }
+// function temp() {
+//     console.log("third");
+// }
+
+//exp:3
+// function temp(val) {
+//     if (val === 0) return
+//     console.log("hello world");
+// }
+// temp(10); //if value is 0 that time // nothing excicuted
+// //hello world
+
+//Question : print hello  world n time
+// function temp(n) {
+//     if (n == 0) return
+//     console.log("hello world");
+//     temp(n - 1);
+// }
+// temp(5);
+// hello world
+// hello world
+// hello world
+// hello world
+// hello world
+
+//exp:2  reverse way
+// function temp(n) {
+//     if (n == 0) return
+//     console.log("hello world");
+//     temp(--n);
+// }
+// temp(5);
+// hello world
+// hello world
+// hello world
+// hello world
+// hello world
+
+//Question : print natural no. 1-n/n-1
+//exp:1
+// function temp(n) {
+//     if(n == 0) return
+//     console.log(n);
+//     temp(n - 1);
+// }
+// temp(10);
+// 10
+// 9
+// 8
+// 7
+// 6
+// 5
+// 4
+// 3
+// 2
+// 1
+
+//Exp:2
+// function temp(n) {
+//     if(n == 0) return
+//     temp(n - 1);
+//     console.log(n);
+// }
+// temp(10);
+// 1
+// 2
+// 3
+// 4
+// 5
+// 6
+// 7
+// 8
+// 9
+// 10
+
+//Question : Factorial / Sum
+
+//f(5) => 5 + f(4) => 4 + f(3) = ans
+
+// function sum(n) {
+//     if (n == 1) return 1;
+//     return n + sum(n - 1);
+// }
+// console.log(sum(10));
+// 55
+
+
+//When should be use retun keyword while recursive call
+
+//Quetion : Fibonacci series
